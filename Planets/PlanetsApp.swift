a40dc4f4f8303cfdec120eb82bb20eebbff5fb7c
seregious <seregious@gmail.com>
2022-05-10 16:06:32 +0300

    Initial Commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Сергей Иванчихин on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
